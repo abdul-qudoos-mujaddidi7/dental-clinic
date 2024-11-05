a3f70cdd17fb5b30bdab7e8c798aba28b6fa148b
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2024-11-05 13:11:05 +0430

    jawad

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,6 +16,7 @@ class ProductResource extends JsonResource
     {
         return
         [
+            'id'=>$this->id,
             'name'=>$this->name,
             'unit'=>$this->unit
         ];
