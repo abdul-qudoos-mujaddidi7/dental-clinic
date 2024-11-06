d7d1d791a9f5e4cf571f4097bbd61b27fab743b2
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2024-11-06 10:40:07 +0430

    jawad

--- app/Http/Resources/ExpenseDetailResource.php
+++ app/Http/Resources/ExpenseDetailResource.php
@@ -19,7 +19,7 @@ class ExpenseDetailResource extends JsonResource
                 'billExpenseId' => $this->bill_expense_id,
                 'productId' => $this->product->id, 
                 'productName' => $this->product->name, 
-                'const' => $this->price,
+                'cost' => $this->price,
                 'quantity' => $this->quantity,
                 'total' => $this->total,
             ];
