60c0149ccebadfed0ef0475194c9be2aa9ce3893
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2024-11-11 12:34:49 +0430

    validation added to doctor buildDelete

--- app/Http/Controllers/DentistController.php
+++ app/Http/Controllers/DentistController.php
@@ -74,7 +74,7 @@ class DentistController extends Controller
 
         $validated= $request->validate([
             "doctorsIds"=>"required|array",
-            "doctorsIds.*"=>"required|exists,dentists,id"
+            "doctorsIds.*"=>"required|exists:dentists,id"
         ]);
 
         Dentist::whereIn('id',$validated['doctorsIds'])->delete();
