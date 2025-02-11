ebb13a4c6d78c36188d79d5ebf6c20ecc10867cc
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2025-02-11 21:36:49 +0430

    ServiceReport added

---
+++ app/Http/Controllers/ServiceReportController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ServiceReportController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        
+        // Check if date range is provided
+        if ($request->has(['from_date', 'to_date']) && !empty($request->from_date) && !empty($request->to_date)) {
+            $fromDate = $request->from_date;
+            $toDate = $request->to_date;
+
+            // Filtered calculations (Custom Data)
+            $filteredData = DB::table('services')
+                ->selectRaw('services.name, COUNT(cure_services.id) as totalApplied')
+                ->join('cure_services', 'cure_services.service_id', '=', 'services.id')
+                // ->join('cures', 'cures.id', '=', 'cure_details.cure_id')
+                ->whereBetween('cures.created_at', [$fromDate, $toDate])
+                ->groupBy('services.id', 'services.name')
+                ->get();
+
+            // Return filtered data
+            return response()->json($filteredData);
+        }
+
+        // Default calculations (All Records)
+        $allData = DB::table('services')
+            ->selectRaw('services.name, COUNT(cure_services.id) as totalApplied')
+            ->join('cure_services', 'cure_services.service_id', '=', 'services.id')
+            ->join('cures', 'cures.id', '=', 'cure_services.cure_id')
+            ->groupBy('services.id', 'services.name')
+            ->get();
+
+        // Return all data
+        return response()->json($allData);
+    }
+}
