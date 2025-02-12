b7ee6777b06f923d092c5dae7bbdcf589eaff468
sardarNadeem <sardar1wadan@gmai.com>
2025-02-12 14:34:58 +0430

    i end the filter fronEnd 50%

--- resources/js/pages/reports/pickup report/PickupReport.vue
+++ resources/js/pages/reports/pickup report/PickupReport.vue
@@ -22,6 +22,26 @@
                         v-model="ReportRepository.pickUpReportSearch"
                     ></v-text-field>
                 </div>
+                <div class="d-flex">
+                    <v-text-field
+                        v-model="formData.fromDate"
+                        class="pl-2"
+                        type="date"
+                        density="compact"
+                        variant="outlined"
+                        label="from date "
+                    ></v-text-field>
+                    <v-text-field
+                        v-model="formData.toDate"
+                        class="px-2"
+                        type="date"
+                        density="compact"
+                        variant="outlined"
+                        label="from date "
+                    ></v-text-field>
+                    <v-btn color="primaryOld" class="px-8" @click="send"> Filter</v-btn>
+                </div>
+                
             </div>
             <!-- v-table server  -->
             <div class="overflow-x-hidden">
@@ -61,10 +81,20 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted,reactive } from "vue";
 import AppBar from "../../../components/AppBar.vue";
 import { useReportRepository } from "@/store/ReportRepository";
 const ReportRepository = useReportRepository();
+const formData = reactive({
+    fromDate:"",
+    toDate:"",
+})
+
+const send =()=>{
+    ReportRepository.fetchServiceReports(formData.fromDate,formData.toDate)
+    console.log(formData,'this is what jawad agha need ')
+
+}
 
 // header
 const headers = [
--- resources/js/pages/reports/services/Services.vue
+++ resources/js/pages/reports/services/Services.vue
@@ -22,6 +22,25 @@
                         v-model="ReportRepository.serviceReportSearch"
                     ></v-text-field>
                 </div>
+                <div class="d-flex">
+                    <v-text-field
+                        v-model="formData.fromDate"
+                        class="pl-2"
+                        type="date"
+                        density="compact"
+                        variant="outlined"
+                        label="from date "
+                    ></v-text-field>
+                    <v-text-field
+                        v-model="formData.toDate"
+                        class="px-2"
+                        type="date"
+                        density="compact"
+                        variant="outlined"
+                        label="from date "
+                    ></v-text-field>
+                    <v-btn color="primaryOld" class="px-8" @click="send"> Filter</v-btn>
+                </div>
             </div>
             <!-- v-table server  -->
             <div class="overflow-x-hidden">
@@ -59,11 +78,20 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import AppBar from "../../../components/AppBar.vue";
 import { useReportRepository } from "@/store/ReportRepository";
 const ReportRepository = useReportRepository();
+const formData = reactive({
+    fromDate:"",
+    toDate:"",
+})
 
+const send =()=>{
+    ReportRepository.fetchServiceReports(formData.fromDate,formData.toDate)
+    console.log(formData,'this is what jawad agha need ')
+
+}
 // header
 const headers = [
     { title: "Service Name", key: "name", align: "start", sortable: false },
