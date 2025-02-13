a2786224e5f0795e4cff6cd18892b61031574cb5
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2025-02-13 19:34:12 +0430

    Filter added

--- resources/js/pages/reports/Expense Product report/ExpenseProductReport.vue
+++ resources/js/pages/reports/Expense Product report/ExpenseProductReport.vue
@@ -23,31 +23,11 @@
                     ></v-text-field>
                 </div>
                 <div class="d-flex">
-<<<<<<< HEAD
-                    <v-text-field
-                        v-model="formData.fromDate"
-                        class="pl-2"
-                        type="date"
-                        density="compact"
-                        variant="outlined"
-                        label="from date "
-                    ></v-text-field>
-                    <v-text-field
-                        v-model="formData.toDate"
-                        class="px-2"
-                        type="date"
-                        density="compact"
-                        variant="outlined"
-                        label="To date "
-                    ></v-text-field>
-                    <v-btn color="primaryOld" class="px-8" @click="send"> Filter</v-btn>
-=======
                     <date-picker
                         v-model:value="ReportRepository.productDateRange"
                         @change="onDateChange"
                         range
                     ></date-picker>
->>>>>>> 0f71dde860ac474288aa3805c11247124473617d
                 </div>
             </div>
             <!-- v-table server  -->
@@ -88,11 +68,7 @@
 </template>
 
 <script setup>
-<<<<<<< HEAD
-import { ref, onMounted,reactive } from "vue";
-=======
 import { ref, onMounted,reactive, watch } from "vue";
->>>>>>> 0f71dde860ac474288aa3805c11247124473617d
 import AppBar from "../../../components/AppBar.vue";
 import { useReportRepository } from "@/store/ReportRepository";
 const ReportRepository = useReportRepository();
@@ -100,18 +76,6 @@ import DatePicker from "vue-datepicker-next";
 import "vue-datepicker-next/index.css";
 const productDateRange = ref([new Date(), new Date()]);
 
-<<<<<<< HEAD
-const formData = reactive({
-    fromDate:"",
-    toDate:"",
-})
-
-const send =()=>{
-    ReportRepository.fetchServiceReports(formData.fromDate,formData.toDate)
-    console.log(formData,'this is what jawad agha need ')
-
-}
-=======
 
 const onDateChange = () => {
     const [startDate, endDate] = ReportRepository.productDateRange;
@@ -138,7 +102,6 @@ onMounted(() => {
     );
     console.log(productDateRange.value[0], productDateRange.value[1], "service report");
 });
->>>>>>> 0f71dde860ac474288aa3805c11247124473617d
 // header
 const headers = [
     { title: "Category Name", key: "name", align: "start", sortable: false },
