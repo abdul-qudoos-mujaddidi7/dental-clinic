beb1983d20733095d01fe43a23c2c15bf7e3b609
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2025-04-13 14:01:32 +0430

    jawad

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SalaryRequest;
 use App\Http\Resources\SalaryResource;
 use App\Models\Salary;
-use App\Http\Resources\SalaryResource;
 use Illuminate\Http\Request;
 
 class SalaryController extends Controller
