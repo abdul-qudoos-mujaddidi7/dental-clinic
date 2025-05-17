085968698646ad79a42936343a298822b95472bc
Jawad Mujaddidi <abdulqudoosmujaddidi7@gmail.com>
2025-05-17 20:25:30 +0430

    Report Created

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,20 +23,28 @@ class DashboardController extends Controller
         $endOfMonth = Carbon::now()->endOfMonth();
         $startOfLastMonth = Carbon::now()->subMonth()->startOfMonth();
         $endOfLastMonth = Carbon::now()->subMonth()->endOfMonth();
-        $currentJalaliDateTime = Jalalian::now()->format('Y-m-d');
+
+        // Jalali equivalents
+        $jalaliToday = Jalalian::fromCarbon($today)->format('Y-m-d');
+        $jalaliStartOfMonth = Jalalian::fromCarbon($startOfMonth)->format('Y-m-d');
+        $jalaliEndOfMonth = Jalalian::fromCarbon($endOfMonth)->format('Y-m-d');
+        $jalaliStartOfLastMonth = Jalalian::fromCarbon($startOfLastMonth)->format('Y-m-d');
+        $jalaliEndOfLastMonth = Jalalian::fromCarbon($endOfLastMonth)->format('Y-m-d');
+        $currentJalaliYear = Jalalian::now()->format('Y-m-d');
+
 
         // Earnings and Expenses for Today
-        $todayEarning = CurePayment::whereDay('date', $today)
+        $todayEarning = CurePayment::whereDay('date', $jalaliToday)
             ->whereNull('deleted_at')
             ->sum('amount');
 
-        $todayExpense = Expense::whereDay('date', $today)
+        $todayExpense = Expense::whereDay('date', $jalaliToday)
             ->whereNull('deleted_at')
             ->sum('amount');
 
         // Total today's expenses including bill expenses
         $todayBillExpense = DB::table('bill_expenses')
-            ->whereDay('created_at', $today)
+            ->whereDay('created_at', $jalaliToday)
             ->whereNull('deleted_at')
             ->sum('grand_total');
 
@@ -57,18 +65,18 @@ class DashboardController extends Controller
         $netProfit = $totalEarnings - $totalAllExpenses;
 
         // Monthly Earnings and Expenses
-        $monthlyEarnings = CurePayment::whereBetween('date', [$startOfMonth, $endOfMonth])
-            ->whereYear('created_at', $currentYear)
+        $monthlyEarnings = CurePayment::whereBetween('date', [$jalaliStartOfMonth, $jalaliEndOfMonth])
+            ->whereYear('created_at', $currentJalaliYear)
             ->whereNull('deleted_at')
             ->sum('amount');
 
-        $monthlyExpenses = Expense::whereBetween('created_at', [$startOfMonth, $endOfMonth])
-            ->whereYear('created_at', $currentYear)
+        $monthlyExpenses = Expense::whereBetween('date', [$jalaliStartOfMonth, $jalaliEndOfMonth])
+            ->whereYear('date',  $currentJalaliYear)
             ->whereNull('deleted_at')
             ->sum('amount');
 
-        $monthlyBillableExpenses = BillExpense::whereBetween('created_at', [$startOfMonth, $endOfMonth])
-            ->whereYear('created_at', $currentYear)
+        $monthlyBillableExpenses = BillExpense::whereBetween('bill_date', [$jalaliStartOfMonth, $jalaliEndOfMonth])
+            ->whereYear('bill_date', $currentJalaliYear)
             ->whereNull('deleted_at')
             ->sum('grand_total');
 
@@ -80,20 +88,20 @@ class DashboardController extends Controller
 
 
         // Earnings for Last Month
-        $lastMonthEarnings = CurePayment::whereBetween('date', [$startOfLastMonth, $endOfLastMonth])
+        $lastMonthEarnings = CurePayment::whereBetween('date', [$jalaliStartOfMonth, $endOfLastMonth])
             ->whereYear('created_at', $currentYear)
             ->whereNull('deleted_at')
             ->sum('amount');
 
         // Expenses for Last Month
-        $lastMonthExpenses = Expense::whereBetween('created_at', [$startOfLastMonth, $endOfLastMonth])
-            ->whereYear('created_at', $currentYear)
+        $lastMonthExpenses = Expense::whereBetween('date', [$jalaliStartOfMonth, $endOfLastMonth])
+            ->whereYear('created_at', $currentJalaliYear)
             ->whereNull('deleted_at')
             ->sum('amount');
 
         // Billable Expenses for Last Month
-        $lastMonthBillableExpenses = BillExpense::whereBetween('created_at', [$startOfLastMonth, $endOfLastMonth])
-            ->whereYear('created_at', $currentYear)
+        $lastMonthBillableExpenses = BillExpense::whereBetween('bill_date', [$jalaliStartOfMonth, $endOfLastMonth])
+            ->whereYear('bill_date',  $currentJalaliYear)
             ->whereNull('deleted_at')
             ->sum('grand_total');
 
@@ -112,8 +120,8 @@ class DashboardController extends Controller
         $dailyExpenses = DB::table('expenses')
             ->join('expense_categories', 'expenses.expense_category_id', '=', 'expense_categories.id')
             ->selectRaw('expense_categories.name as categoryName, SUM(expenses.amount) as totalExpense, 
-         (SUM(expenses.amount) / (SELECT SUM(amount) FROM expenses WHERE DATE(created_at) = ?)) * 100 as percentage', [$today])
-            ->whereDate('expenses.date', $today)
+         (SUM(expenses.amount) / (SELECT SUM(amount) FROM expenses WHERE DATE(created_at) = ?)) * 100 as percentage', [$jalaliToday])
+            ->whereDate('expenses.date', $jalaliToday)
             ->groupBy('expense_categories.name')
             ->orderBy('totalExpense', 'desc')
             ->get();
@@ -122,9 +130,9 @@ class DashboardController extends Controller
         $monthlyExpenses = DB::table('expenses')
             ->join('expense_categories', 'expenses.expense_category_id', '=', 'expense_categories.id')
             ->selectRaw('expense_categories.name as categoryName, SUM(expenses.amount) as totalExpense, 
-         (SUM(expenses.amount) / (SELECT SUM(amount) FROM expenses WHERE YEAR(created_at) = ? AND created_at BETWEEN ? AND ?)) * 100 as percentage', [now()->year, $startOfMonth, $endOfMonth])
-            ->whereYear('expenses.created_at', now()->year)
-            ->whereBetween('expenses.date', [$startOfMonth, $endOfMonth])
+         (SUM(expenses.amount) / (SELECT SUM(amount) FROM expenses WHERE YEAR(created_at) = ? AND created_at BETWEEN ? AND ?)) * 100 as percentage', [now()->year, $jalaliStartOfMonth, $jalaliEndOfMonth])
+            ->whereYear('expenses.created_at',  $currentJalaliYear)
+            ->whereBetween('expenses.date', [$jalaliStartOfMonth, $jalaliEndOfMonth])
             ->groupBy('expense_categories.name')
             ->orderBy('totalExpense', 'desc')
             ->get();
@@ -133,8 +141,8 @@ class DashboardController extends Controller
         $yearlyExpenses = DB::table('expenses')
             ->join('expense_categories', 'expenses.expense_category_id', '=', 'expense_categories.id')
             ->selectRaw('expense_categories.name as categoryName, SUM(expenses.amount) as totalExpense, 
-         (SUM(expenses.amount) / (SELECT SUM(amount) FROM expenses WHERE YEAR(created_at) = ?)) * 100 as percentage', [$currentYear])
-            ->whereYear('expenses.date', $currentYear)
+         (SUM(expenses.amount) / (SELECT SUM(amount) FROM expenses WHERE YEAR(created_at) = ?)) * 100 as percentage', [$currentJalaliYear])
+            ->whereYear('expenses.date',  $currentJalaliYear)
             ->groupBy('expense_categories.name')
             ->orderBy('totalExpense', 'desc')
             ->get();
@@ -143,7 +151,7 @@ class DashboardController extends Controller
         $upcomingAppointments = DB::table('appointments')
             ->join('people', 'appointments.people_id', '=', 'people.id')
             ->select('people.name', 'people.phone', DB::raw('TIME(appointments.date_time) as time'))
-            ->whereDate('appointments.date_time','>=', $currentJalaliDateTime)
+            ->whereDate('appointments.date_time', '>=', $currentJalaliYear)
             ->orderBy('time', 'asc')
             ->limit(5)
             ->get();
@@ -156,19 +164,19 @@ class DashboardController extends Controller
             // Calculate total expenses for the month
             $expenseAmount = DB::table('expenses')
                 ->whereMonth('date', $i + 1)
-                ->whereYear('date', $currentYear) // Filter by current year
+                ->whereYear('date',  $currentJalaliYear) // Filter by current year
                 ->sum('amount');
 
             // Calculate total bill expenses for the month
             $billExpenseAmount = DB::table('bill_expenses')
                 ->whereMonth('created_at', $i + 1)
-                ->whereYear('bill_date', $currentYear) // Filter by current year
+                ->whereYear('bill_date', $currentJalaliYear) // Filter by current year
                 ->sum('grand_total');
 
             // Calculate total income for the month
             $incomeAmount = DB::table('cure_payments') // Assuming you have an 'incomes' table
                 ->whereMonth('date', $i + 1)
-                ->whereYear('date', $currentYear) // Filter by current year
+                ->whereYear('date',  $currentJalaliYear) // Filter by current year
                 ->sum('amount');
 
             $everyMonthIncomes[$i] = $incomeAmount;
